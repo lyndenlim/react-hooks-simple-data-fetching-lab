7a4913dfa8adf42b26a83afa12d0100465273947
Lynden Lim <lyndenlim15@gmail.com>
2022-02-04 20:42:47 -0500

    commit tree 01b59b1c995f45746d44d7e05262e0c12def8973

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [dogs, setDogs] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(resp => resp.json())
+            .then(dogData => {
+                console.log(dogData)
+                setDogs(dogData)
+                setIsLoaded(true)
+            })
+    }, [])
+
+    if (!isLoaded) {
+        return <p>Loading...</p>
+    }
+
+    return <img src={dogs.message} alt="A Random Dog"/>
+
+}
+
+export default App
